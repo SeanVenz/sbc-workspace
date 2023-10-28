7e5b1459bf4a5a4faeb37497ae1920efc6fb7414
SeanVenz <seanvenz.quijano@cit.edu>
2023-10-28 13:11:58 +0800

    export function

--- 01-system-program/01-read-balance.ts
+++ 01-system-program/01-read-balance.ts
@@ -1,10 +1,8 @@
 import * as Web3 from '@solana/web3.js';
 
-async function main(){
+export async function getBalance(){
     const publicKey = new Web3.PublicKey('4spmH3EniPoR57szbmXGDb2nW4wW2t4ipa8x6CvvF93m');
     const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
     const balance = await connection.getBalance(publicKey);
-    console.log('Balance: ', balance);
+    console.log('Your SOL balance is: ', balance);
 }
-
-main();
